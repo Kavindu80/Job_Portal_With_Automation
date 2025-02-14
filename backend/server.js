dc76de4666932710aef97eb9cc2457b1cd424edc
Kavindu Liyanage <95487550+Kavindu80@users.noreply.github.com>
2025-02-14 18:36:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,12 @@ import app from "./app.js";
 import cloudinary from "cloudinary";
 import cors from "cors";
 
+app.use(cors({
+  origin: 'https://job-portal-fawn-phi.vercel.app', // Replace with your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
